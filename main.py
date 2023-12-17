5c9242d27bc736f98bfeabf76a0200314f1fca3d
user <dada.sasha1234567@gmail.com>
2023-12-17 12:42:50 +0200

    dz_6

---
+++ main.py
@@ -0,0 +1,28 @@
+result = []
+
+def divider(a, b):
+    try:
+        if a < b:
+            raise ValueError("a should be greater than or equal to b")
+        if b == 0:
+            raise ZeroDivisionError("division by zero is not allowed")
+        if b > 100:
+            raise IndexError("b should be less than or equal to 100")
+        return a / b
+    except ValueError as ve:
+        print(f"ValueError: {ve}")
+    except ZeroDivisionError as zde:
+        print(f"ZeroDivisionError: {zde}")
+    except IndexError as ie:
+        print(f"IndexError: {ie}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8: 4}
+
+for key in data:
+    res = divider(key, data[key])
+    result.append(res)
+
+print(result)
+t)
